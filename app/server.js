b2ddc20a74b44d057d30592889e8e23e71059c92
ics-konno <konno@ics-web.jp>
2024-04-18 13:48:03 +0900

    HTMLファイルのインデントを修正
    インデントが不統一だったHTMLファイルを修正しました。この変更により、各HTMLファイルのコードの可読性が向上します。また、server.jsファイル中のレンダリング文字列のスペーシングも調整しました。

--- app/server.js
+++ app/server.js
@@ -16,7 +16,7 @@ const server = http.createServer((request, response) => {
                 view += '<p>メールアドレス： '+ params.get('email') + '</p>';
                 view += '<p>電話番号： '+ params.get('tel') + '</p>';
                 view += '<p>性別： '+ params.get('gender') + '</p>';
-                view += '<p>生年月日： '+ params.get('birthday') + '</p>';
+                view += '<p>生年月日： ' + params.get('birthday') + '</p>';
                 view += '<p>志望動機： '+ params.get('reason') + '</p>';
                 view += '<p>ポートフォリオ： '+ params.get('portfolio') + '</p>';
                 view += '<p>その他： '+ params.get('other') + '</p>';
