73e5e795f0d96d50e690576ca0062093d8a72d22
ics-ikeda <ikeda@ics-web.jp>
2025-08-26 15:20:06 +0900

    レガシーコードの改善

--- app/server.js
+++ app/server.js
@@ -1,28 +1,31 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        "Content-Type": "text/html; charset=utf-8",
+  response.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+  });
+
+  if ("/submit" == request.url && "POST" == request.method) {
+    let requestData = "";
+    let responseView = "";
+
+    request.on("data", (chunk) => {
+      requestData += chunk;
     });
-        if ('/submit' == request.url && 'POST' == request.method) {
-            let data = "";
-            let view = "";
-            request.on("data", function (_argdata) {
-                data += _argdata;
-            });
-            request.on("end", function() {
-                const params = new URLSearchParams(data);
-                view += '<p>氏名： '+ params.get('name') + '</p>';
-                view += '<p>メールアドレス： '+ params.get('email') + '</p>';
-                view += '<p>電話番号： '+ params.get('tel') + '</p>';
-                view += '<p>性別： '+ params.get('gender') + '</p>';
-                view += '<p>生年月日： ' + params.get('birthday') + '</p>';
-                view += '<p>志望動機： '+ params.get('reason') + '</p>';
-                view += '<p>ポートフォリオ： '+ params.get('portfolio') + '</p>';
-                view += '<p>その他： '+ params.get('other') + '</p>';
-                response.end(view);
-            })
-        }
+
+    request.on("end", () => {
+      const params = new URLSearchParams(requestData);
+      responseView += "<p>氏名： " + params.get("name") + "</p>";
+      responseView += "<p>メールアドレス： " + params.get("email") + "</p>";
+      responseView += "<p>電話番号： " + params.get("tel") + "</p>";
+      responseView += "<p>性別： " + params.get("gender") + "</p>";
+      responseView += "<p>生年月日： " + params.get("birthday") + "</p>";
+      responseView += "<p>志望動機： " + params.get("reason") + "</p>";
+      responseView += "<p>ポートフォリオ： " + params.get("portfolio") + "</p>";
+      responseView += "<p>その他： " + params.get("other") + "</p>";
+      response.end(responseView);
+    });
+  }
 });
 
 server.listen(3000);
